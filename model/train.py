8fd23afebe88ddad778a7cebb41001131040a5df
Muralijayagopi <jmurali.krishnann@gmail.com>
2025-05-30 21:28:30 +0000

    updated train.py

--- model/train.py
+++ model/train.py
@@ -1,19 +1,22 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 import pickle
-from preprocessing import preprocess_data
+import os
+
+from model.preprocessing import preprocess_data  # if used
 
 # Load and preprocess data
 df = pd.read_csv('data/sample_data.csv')
-df = preprocess_data(df)
-
 X = df[['YearsExperience']]
 y = df['Salary']
 
-# Train model
 model = LinearRegression()
 model.fit(X, y)
 
-# Save model
+# Ensure the model directory exists
+os.makedirs('model', exist_ok=True)
+
+# Save the trained model
 with open('model/model.pkl', 'wb') as f:
     pickle.dump(model, f)
+
